273b467212f9fe0ddf4aa2c8183c222c70fdf3ad
Dylan Yang & Rakey Drammeh <pair+dylanlovescoffee.rdrammeh@devbootcamp.com>
2017-06-29 16:06:04 -0400

    Landing page will show each question, the question's score/votes, and count of answers.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,7 @@
 get '/' do
   erb :'index'
 end
+
+get '/404' do
+  # erb error
+end
---
+++ app/controllers/questions.rb
@@ -0,0 +1,19 @@
+get '/questions' do
+  # erb questions/index
+end
+
+get '/questions/new' do
+  # erb questions/new
+end
+
+post '/questions' do
+  # erb questions/index
+end
+
+get '/questions/:id' do
+  # erb: show
+end
+
+delete '/questions/:id' do
+  # erb questions/_delete
+end
