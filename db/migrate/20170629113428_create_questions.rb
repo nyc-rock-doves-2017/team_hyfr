6c25c507a88929307c44eaf95738fba91cc8f6b0
kevin & Iftekhar Alam <pair+kevinhcross96.rahat64@devbootcamp.com>
2017-06-29 14:42:30 -0400

    Create seeds and make logical connections between associations

--- db/migrate/20170629113428_create_questions.rb
+++ db/migrate/20170629113428_create_questions.rb
@@ -2,6 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.integer :user_id, NULL: false
+      t.string :title, NULL: false
       t.text :body, NULL: false
     end
   end
