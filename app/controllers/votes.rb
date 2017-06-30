d93c16fd48c43bbad7d7fa43f6df71714322754e
Dylan Yang & kevin & Iftekhar Alam & Rakey Drammeh <pair+dylanlovescoffee.kevinhcross96.rahat64.rdrammeh@devbootcamp.com>
2017-06-30 14:01:42 -0400

    Restrict user from upvoting downvoting more than once.

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,67 +1,57 @@
+def upvote_valid(post)
+    vote_array = post.votes.select do |vote|
+    vote.user_id == session[:user_id]
+  end
 
+  if vote_array.empty?
+    logger = current_user
+      post.votes.create(user_id: logger.id, value: 1)
+    redirect back
+  else
+    redirect back
+  end
+end
+
+def downvote_valid(post)
+    vote_array = post.votes.select do |vote|
+    vote.user_id == session[:user_id]
+  end
+
+  if vote_array.empty?
+    logger = current_user
+      post.votes.create(user_id: logger.id, value: -1)
+    redirect back
+  else
+    redirect back
+  end
+end
 
 post '/posts/:id/vote' do
-
-# One vote per user per Questions and Answer and Comment
-# all_ans = Answer.all
-# all_ans.each do |ans|
-#   individual_votes = ans.votes.select do |vote|
-#     vote.user_id == sessions[:user_id]
-#   end
-#   if individual_votes.empty?
-#     ALLOW BUTTON CLICKY
-#   else
-#     DONT ALLOW BUTTON CLICKY
-#   end
-# end
   post = Question.find(params[:id])
-  logger = current_user
-  post.votes.create(user_id: logger.id, value: 1)
-  redirect back
-
+  upvote_valid(post)
 end
 
 post '/posts/:id/downvote' do
-
   post = Question.find(params[:id])
-  logger = current_user
-  post.votes.create(user_id: logger.id, value: -1)
-  redirect back
-
+  downvote_valid(post)
 end
 
 post '/posts/:id/vote/answer' do
-
   post = Answer.find(params[:id])
-  logger = current_user
-  post.votes.create(user_id: logger.id, value: 1)
-  redirect back
-
+  upvote_valid(post)
 end
 
 post '/posts/:id/downvote/answer' do
-
   post = Answer.find(params[:id])
-  logger = current_user
-  post.votes.create(user_id: logger.id, value: -1)
-  redirect back
-
+  downvote_valid(post)
 end
 
 post '/posts/:id/vote/comment' do
-
   post = Comment.find(params[:id])
-  logger = current_user
-  post.votes.create(user_id: logger.id, value: 1)
-  redirect back
-
+  upvote_valid(post)
 end
 
 post '/posts/:id/downvote/comment' do
-
   post = Comment.find(params[:id])
-  logger = current_user
-  post.votes.create(user_id: logger.id, value: -1)
-  redirect back
-
+  downvote_valid(post)
 end
